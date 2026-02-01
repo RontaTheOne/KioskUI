8583d51a93946bb4ce3c970ccbb7a83247272d2b
Ronald Tapias <ronta067@gmail.com>
2026-02-01 00:39:50 -0500

    KioskUI

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import './App.css'
+import Layout from "./components/layout/layout.jsx"
+import Menu from "./pages/Menu.jsx";
+import Checkout from "./pages/Checkout.jsx";
+import OrderSuccess from "./pages/OrderSuccess.jsx";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Navigate to="/menu" replace />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/order-success/:id" element={<OrderSuccess />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/menu" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
