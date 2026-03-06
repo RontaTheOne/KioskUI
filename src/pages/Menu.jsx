afde721d3a2bc31f1df8cdd0084dec105ee91212
Ronald Tapias <ronta067@gmail.com>
2026-03-05 20:19:14 -0500

    feat: Creación Modal detalle del producto

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
+import { useState, useRef } from "react";
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import CartOffcanvas from "../components/Cart/CartOffcanvas";
 function Menu() {
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const modalRef = useRef(null);
+
+  const handleCardClick = (product) => {
+    setSelectedProduct(product);
+    const ms = new bootstrap.Modal(modalRef.current);
+    ms.show();
+  };
+
   return (
     <div className="menu-page">
       <nav className="navbar bg-body-tertiary ustify-content-center">
@@ -11,59 +22,59 @@ function Menu() {
         </div>
       </nav>
 
-      <div class="container py-4">
-        <div class="d-flex justify-content-center gap-4 flex-wrap category-wrapper">
+      <div className="container py-4">
+        <div className="d-flex justify-content-center gap-4 flex-wrap category-wrapper">
           <Link
             to="/bowls"
-            class="category-item active"
+            className="category-item active"
             style={{ textDecoration: "none" }}
           >
-            <div class="category-icon">🥣</div>
+            <div className="category-icon">🥣</div>
             <span className="text-black">Bowls</span>
           </Link>
 
           <Link
             to="/hamburguesas"
-            class="category-item"
+            className="category-item"
             style={{ textDecoration: "none" }}
           >
-            <div class="category-icon">🍔</div>
+            <div className="category-icon">🍔</div>
             <span className="text-black">Hamburguesas</span>
           </Link>
 
           <Link
             to="/hot-dogs"
-            class="category-item"
+            className="category-item"
             style={{ textDecoration: "none" }}
           >
-            <div class="category-icon">🌭</div>
+            <div className="category-icon">🌭</div>
             <span className="text-black">Hot Dogs</span>
           </Link>
 
           <Link
             to="/acompañantes"
-            class="category-item"
+            className="category-item"
             style={{ textDecoration: "none" }}
           >
-            <div class="category-icon">🥟</div>
+            <div className="category-icon">🥟</div>
             <span className="text-black">Acompañantes</span>
           </Link>
 
           <Link
             to="/acompañantes"
-            class="category-item"
+            className="category-item"
             style={{ textDecoration: "none" }}
           >
-            <div class="category-icon">🥤</div>
+            <div className="category-icon">🥤</div>
             <span className="text-black">Bebidas</span>
           </Link>
 
           <Link
             to="/postres"
-            class="category-item"
+            className="category-item"
             style={{ textDecoration: "none" }}
           >
-            <div class="category-icon">🍦</div>
+            <div className="category-icon">🍦</div>
             <span className="text-black">Postres</span>
           </Link>
         </div>
@@ -86,77 +97,57 @@ function Menu() {
         </div>
         <br /> <br />
         <div className="row g-4">
-          <div class="col col-sm-6 col-md-4">
-            <div class="product-card text-center p-3">
-              <img
-                src="https://s7d1.scene7.com/is/image/mcdonalds/DC_202201_0007-005_QuarterPounderwithCheese_1564x1564-1:nutrition-calculator-tile"
-                class="img-fluid product-img mb-2"
-                alt="Big King"
-              />
-              <h6 class="fw-bold mb-1">Big King</h6>
-              <p class="price mb-0">$ 4.10</p>
+          {[
+            {
+              name: "Big King",
+              price: 4.1,
+              img:
+                "https://s7d1.scene7.com/is/image/mcdonalds/DC_202201_0007-005_QuarterPounderwithCheese_1564x1564-1:nutrition-calculator-tile",
+            },
+            {
+              name: "Whopper",
+              price: 4.19,
+              img:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4YDCJqgMFtsD9dBn2uch0RTrROXHmvr1vnw&s",
+            },
+            {
+              name: "Rodeo Whopper",
+              price: 2.99,
+              img: "https://images.rappi.com/restaurants_background/home76-1747260915136.jpg",
+            },
+            {
+              name: "Big King",
+              price: 4.1,
+              img:
+                "https://s7d1.scene7.com/is/image/mcdonalds/DC_202201_0007-005_QuarterPounderwithCheese_1564x1564-1:nutrition-calculator-tile",
+            },
+            {
+              name: "Whopper",
+              price: 4.19,
+              img:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4YDCJqgMFtsD9dBn2uch0RTrROXHmvr1vnw&s",
+            },
+            {
+              name: "Rodeo Whopper",
+              price: 2.99,
+              img: "https://images.rappi.com/restaurants_background/home76-1747260915136.jpg",
+            },
+          ].map((prod, idx) => (
+            <div className="col col-sm-6 col-md-4" key={idx}>
+              <div
+                className="product-card text-center p-3"
+                onClick={() => handleCardClick(prod)}
+              >
+                <img
+                  src={prod.img}
+                  className="img-fluid product-img mb-2"
+                  alt={prod.name}
+                />
+                <h6 className="fw-bold mb-1">{prod.name}</h6>
+                <p className="price mb-0">$ {prod.price.toFixed(2)}</p>
+              </div>
             </div>
-          </div>
-
-          <div class="col col-sm-6 col-md-4">
-            <div class="product-card text-center p-3">
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4YDCJqgMFtsD9dBn2uch0RTrROXHmvr1vnw&s"
-                class="img-fluid product-img mb-2"
-                alt="Whopper"
-              />
-              <h6 class="fw-bold mb-1">Whopper</h6>
-              <p class="price mb-0">$ 4.19</p>
-            </div>
-          </div>
-
-          <div class="col col-sm-6 col-md-4">
-            <div class="product-card text-center p-3">
-              <img
-                src="https://images.rappi.com/restaurants_background/home76-1747260915136.jpg"
-                class="img-fluid product-img mb-2"
-                alt="Rodeo Whopper"
-              />
-              <h6 class="fw-bold mb-1">Rodeo Whopper</h6>
-              <p class="price mb-0">$ 2.99</p>
-            </div>
-          </div>
-
-          <div class="col col-sm-6 col-md-4">
-            <div class="product-card text-center p-3">
-              <img
-                src="https://s7d1.scene7.com/is/image/mcdonalds/DC_202201_0007-005_QuarterPounderwithCheese_1564x1564-1:nutrition-calculator-tile"
-                class="img-fluid product-img mb-2"
-                alt="Big King"
-              />
-              <h6 class="fw-bold mb-1">Big King</h6>
-              <p class="price mb-0">$ 4.10</p>
-            </div>
-          </div>
-
-          <div class="col col-sm-6 col-md-4">
-            <div class="product-card text-center p-3">
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4YDCJqgMFtsD9dBn2uch0RTrROXHmvr1vnw&s"
-                class="img-fluid product-img mb-2"
-                alt="Whopper"
-              />
-              <h6 class="fw-bold mb-1">Whopper</h6>
-              <p class="price mb-0">$ 4.19</p>
-            </div>
-          </div>
-
-          <div class="col col-sm-6 col-md-4">
-            <div class="product-card text-center p-3">
-              <img
-                src="https://images.rappi.com/restaurants_background/home76-1747260915136.jpg"
-                class="img-fluid product-img mb-2"
-                alt="Rodeo Whopper"
-              />
-              <h6 class="fw-bold mb-1">Rodeo Whopper</h6>
-              <p class="price mb-0">$ 2.99</p>
-            </div>
-          </div>
+          ))}
         </div>
         <br />
         <div className="row g-4">
@@ -206,6 +197,143 @@ function Menu() {
         <Link to="/checkout" className="btn btn-danger pay-btn">
           Pagar
         </Link>
+      </div>
+
+      {/* product configuration modal */}
+      <div
+        className="modal fade"
+        id="productModal"
+        ref={modalRef}
+        tabIndex="-1"
+        aria-labelledby="productModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-lg">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="productModalLabel">
+                {selectedProduct ? selectedProduct.name : 'Producto'}
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              {selectedProduct && (
+                <div className="text-center mb-3">
+                  <img
+                    src={selectedProduct.img}
+                    className="img-fluid product-img mb-2"
+                    alt={selectedProduct.name}
+                    style={{ maxHeight: 150 }}
+                  />
+                  <p className="fw-bold mb-1">
+                    $ {selectedProduct.price.toFixed(2)}
+                  </p>
+                </div>
+              )}
+              <div className="mb-3">
+                <h6>Tamaño</h6>
+                <div className="btn-group" role="group" aria-label="size">
+                  <input
+                    type="radio"
+                    className="btn-check"
+                    name="size"
+                    id="sizeSmall"
+                    autoComplete="off"
+                  />
+                  <label className="btn btn-outline-primary" htmlFor="sizeSmall">
+                    Pequeño
+                  </label>
+
+                  <input
+                    type="radio"
+                    className="btn-check"
+                    name="size"
+                    id="sizeMedium"
+                    autoComplete="off"
+                    defaultChecked
+                  />
+                  <label
+                    className="btn btn-outline-primary"
+                    htmlFor="sizeMedium"
+                  >
+                    Mediano
+                  </label>
+
+                  <input
+                    type="radio"
+                    className="btn-check"
+                    name="size"
+                    id="sizeLarge"
+                    autoComplete="off"
+                  />
+                  <label className="btn btn-outline-primary" htmlFor="sizeLarge">
+                    Grande
+                  </label>
+                </div>
+              </div>
+              <div className="mb-3">
+                <h6>Quita algo</h6>
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="noOnion"
+                  />
+                  <label className="form-check-label" htmlFor="noOnion">
+                    Sin cebolla
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="noLettuce"
+                  />
+                  <label className="form-check-label" htmlFor="noLettuce">
+                    Sin lechuga
+                  </label>
+                </div>
+              </div>
+              <div className="mb-3">
+                <h6>Añadir un ingrediente</h6>
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="addCheese"
+                  />
+                  <label className="form-check-label" htmlFor="addCheese">
+                    Queso extra
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="addBacon"
+                  />
+                  <label className="form-check-label" htmlFor="addBacon">
+                    Tocino
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-primary">
+                Agregar
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
 
       <CartOffcanvas total="33.02">
